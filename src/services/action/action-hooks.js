120cee4e80dc1cb9f2aba02e17c73aa9c94666d8
forafish <fora.luo@gmail.com>
2018-03-17 23:58:42 +0800

    add cache to action service

--- src/services/action/action-hooks.js
+++ src/services/action/action-hooks.js
@@ -1,11 +1,14 @@
 import { hooks } from 'mostly-feathers-mongoose';
+import { cache } from 'mostly-feathers-cache';
+
 import ActionEntity from '~/entities/action-entity';
 
 module.exports = function(options = {}) {
   return {
     before: {
       all: [
-        hooks.authenticate('jwt', options.auth)
+        hooks.authenticate('jwt', options.auth),
+        cache(options.cache)
       ],
       update: [
         hooks.discardFields('id', 'createdAt', 'updatedAt', 'destroyedAt')
@@ -17,6 +20,7 @@ module.exports = function(options = {}) {
     after: {
       all: [
         hooks.populate('rules.rewards.metric', { service: 'metrics' }),
+        cache(options.cache),
         hooks.presentEntity(ActionEntity, options),
         hooks.responder()
       ]
