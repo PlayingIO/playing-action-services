fabdae1d63f217f6b01ca0a82ade5b0ab14412f4
forafish <fora.luo@gmail.com>
2018-03-19 17:30:54 +0800

    Fix fulfillActionRequires

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,7 @@ import { helpers } from 'mostly-feathers-mongoose';
 import { helpers as rules } from 'playing-rule-services';
 
 export const fulfillActionRequires = (action, user) => {
-  return rules.fulfillRequires(action.rules, action.variables, user);
+  return rules.fulfillRequires(user, action.variables, action.requires);
 };
 
 export const fulfillActionRewards = (action, user) => {
@@ -11,11 +11,11 @@ export const fulfillActionRewards = (action, user) => {
 };
 
 export const getActionRewards = (action) => {
-  return fp.flatten(fp.map(rule => {
+  return fp.flatMap(rule => {
     return (rule.rewards || []).map(reward => {
       reward.type = reward.metric && reward.metric.type;
       reward.metric = reward.metric && helpers.getId(reward.metric);
       return reward;
     });
-  }, action.rules));
+  }, action.rules);
 };
