845bfa366304cea5e54640986354371541e534d6
forafish <fora.luo@gmail.com>
2018-03-27 03:08:00 +0800

    use feeds.addActivity

--- src/services/user-action/user-action.events.js
+++ src/services/user-action/user-action.events.js
@@ -1,9 +1,11 @@
 import makeDebug from 'debug';
-import { helpers as feeds } from 'playing-feed-services';
 
 const debug = makeDebug('playing:action-services:action:events');
 
 const createActivity = async function (app, userAction, verb, message) {
+  const svcFeeds = app.service('feeds');
+  const svcNotificationFeeds = app.service('notification-feeds');
+
   const activity = {
     actor: `user:${userAction.user}`,
     verb: verb,
@@ -16,11 +18,10 @@ const createActivity = async function (app, userAction, verb, message) {
 
   await Promise.all([
     // add to player's activity log
-    feeds.createActivity(app, 'feeds')(`user:${userAction.user}`, activity),
+    svcFeeds.action('addActivity').patch(`user:${userAction.user}`, activity),
     // add to notification stream of the player
-    feeds.createActivity(app, 'notifications')(`user:${userAction.user}`, activity),
+    //svcNotificationFeeds.action('addActivity').patch(`user:${userAction.user}`, activity)
   ]);
-
 };
 
 // subscribe to action.play events
@@ -28,12 +29,12 @@ export default function (app, options) {
   app.trans.add({
     pubsub$: true,
     topic: 'playing.events',
-    cmd: 'action.play'
+    cmd: 'action.played'
   }, (resp) => {
     const userAction = resp.event;
     if (userAction) {
-      debug('action.play event', userAction.action, userAction.user);
-      createActivity(app, userAction, 'action.play', 'Playing an action');
+      debug('action.played event', userAction.action, userAction.user);
+      createActivity(app, userAction, 'action.played', 'Playing an action');
     }
   });
 }
