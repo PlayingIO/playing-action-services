bd5c562250bd60027f1fbf8d417f47c09a523af3
forafish <fora.luo@gmail.com>
2018-04-23 19:13:29 +0800

    parse action.varibles before use it

--- src/helpers.js
+++ src/helpers.js
@@ -3,9 +3,11 @@ import { helpers } from 'mostly-feathers-mongoose';
 import { helpers as rules } from 'playing-rule-services';
 
 export const fulfillActionRequires = (action, user) => {
-  return rules.fulfillRequires(user, action.variables, action.requires);
+  const variables = rules.parseVariables(action.variables);
+  return rules.fulfillRequires(user, variables, action.requires);
 };
 
 export const fulfillActionRewards = (action, user) => {
-  return rules.fulfillCustomRewards(action.rules, action.variables, user);
+  const variables = rules.parseVariables(action.variables);
+  return rules.fulfillCustomRewards(action.rules, variables, user);
 };
