ce6ab129e75c83f79749d28c1dfe1ff76f65a480
forafish <fora.luo@gmail.com>
2018-05-10 19:20:28 +0800

    user-action notifiers

---
+++ src/services/user-action/user-action.notifiers.js
@@ -0,0 +1,27 @@
+import { helpers } from 'mostly-feathers-mongoose';
+import { helpers as feeds } from 'playing-feed-services';
+
+import { createActionActivity } from '../../helpers';
+
+// play action activity
+const playAction = (context) => {
+  const userAction = helpers.getHookData(context);
+  const actor = context.params.user.id;
+  const custom = {
+    actor: `user:${actor}`,
+    verb: 'action.play',
+    message: 'Play an action',
+    count: userAction.count,
+    rewards: userAction.rewards
+  };
+  return [
+    createActionActivity(context, userAction, custom),
+    `user:${actor}`,                 // add to player's activity log
+    `notification:${actor}`          // add to player's notification stream
+  ];
+};
+
+export default {
+  'action.play': playAction
+};
+
