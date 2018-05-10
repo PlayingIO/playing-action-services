00f8a566e72c638d7a4107aa116853b6cfab2c89
forafish <fora.luo@gmail.com>
2018-05-11 04:35:59 +0800

    Fix call action method

--- src/services/user-action/user-action.service.js
+++ src/services/user-action/user-action.service.js
@@ -1,6 +1,5 @@
 import assert from 'assert';
 import makeDebug from 'debug';
-import { idAction } from 'mostly-feathers';
 import { Service, helpers, createService } from 'mostly-feathers-mongoose';
 import fp from 'mostly-func';
 import { helpers as metrics } from 'playing-metric-services';
@@ -40,10 +39,8 @@ export class UserActionService extends Service {
    * get user actions by action id
    */
   async get (id, params) {
-    let action = null;
-    [id, action] = idAction(id, params);
-    if (action) {
-      return super._action('get', action, id, null, params);
+    if (this._isAction(id, params)) {
+      return super._action('get', id, null, params);
     }
 
     params = fp.assign({ query: {} }, params);
@@ -110,7 +107,7 @@ export class UserActionService extends Service {
   /**
    * Active actions for current player
    */
-  async active (id, data, params) {
+  async active (params) {
     params = fp.assign({ query: {} }, params);
     assert(params.user, 'params.user not provided');
 
