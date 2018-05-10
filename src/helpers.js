7ed11f9b3c23fc8771a3fcfe1480f0cc691cbd11
forafish <fora.luo@gmail.com>
2018-05-10 19:20:40 +0800

    add createActionActivity

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,4 @@
+import { helpers } from 'mostly-feathers-mongoose';
 import { helpers as rules } from 'playing-rule-services';
 
 export const fulfillActionRequires = (action, user) => {
@@ -8,4 +9,17 @@ export const fulfillActionRequires = (action, user) => {
 export const fulfillActionRewards = (action, user) => {
   const variables = rules.parseVariables(action.variables);
   return rules.fulfillCustomRewards(action.rules, variables, user);
+};
+
+// create a user action activity
+export const createActionActivity = (context, userAction, custom) => {
+  const actor = helpers.getId(userAction.user);
+  const action = helpers.getId(userAction.action);
+  return {
+    actor: `user:${actor}`,
+    object: `action:${action}`,
+    foreignId: `userAction:${userAction.id}`,
+    time: new Date().toISOString(),
+    ...custom
+  };
 };
